f3525e097fa182c3a7b3db889f3392b9605daee7
troy Stribling <troy.stribling@gmail.com>
2015-08-22 17:39:36 -0700

    created project

---
+++ MemoryMappedFileSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemoryMappedFileSwift
+//
+//  Created by Troy Stribling on 8/22/15.
+//  Copyright © 2015 Troy Stribling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
